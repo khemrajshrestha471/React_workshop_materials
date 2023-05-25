4de504e628051cd01ac64aa2a08e84b1eeb66ded
Khemraj Shrestha <86350457+khemrajshrestha471@users.noreply.github.com>
2023-05-25 17:43:47 +0545

    Update Navbar.js

--- props/components/Navbar.js
+++ props/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ReactPropTypes } from 'react'
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
